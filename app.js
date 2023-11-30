0297483515b8b0a65663c06413456eff452b29ba
Oguti David Brian <39586345+osppro@users.noreply.github.com>
2023-11-30 20:34:08 +0300

    code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Var app = angular.module(‘myApp’,[]);
 
 app.controller('FirstController', function($scope) {
-$scope.message = 'Hello from FirstController';
+$scope.message = 'Testing my Angular App';
 });
